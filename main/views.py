4953d5f608652d1de67ad8802585daf00ae97326
Elina <babulekh010@gmail.com>
2022-05-26 06:24:29 -0400

    i did my home work

---
+++ main/views.py
@@ -0,0 +1,39 @@
+from rest_framework.views import APIView
+from rest_framework import status, generics
+from rest_framework.response import Response
+
+from main.serializers import ProductSerializer
+from django.db.models import Q
+from .models import Product
+
+
+
+class ProductAPIView(APIView):
+
+    def get(self, request):
+        search = request.query_params.get('search')
+        if search:
+            products = Product.objects.filter(Q(title__icontains=search) | Q(price__icontains=search), is_published=True)
+        else:
+            products = Product.objects.filter(is_published=True).order_by('title')
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class RetrieveProductView(generics.RetrieveAPIView):
+    queryset = Product.objects.all() # Ленивый запрос
+    serializer_class = ProductSerializer
+
+
+class DestroyProductView(generics.DestroyAPIView):
+    queryset = Product.objects.all() # Ленивый запрос
+    serializer_class = ProductSerializer
+
+
+class UpdateProductView(generics.UpdateAPIView):
+    queryset = Product.objects.all() # Ленивый запрос
+    serializer_class = ProductSerializer
+
+
+class CreateProductView(generics.CreateAPIView):
+    queryset = Product.objects.all() # Ленивый запрос
+    serializer_class = ProductSerializer
